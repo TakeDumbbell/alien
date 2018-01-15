3233ef01b025687776234777c44d942d9aa44ac7
陈环宇 <2399644890@qq.com>
2018-01-15 23:24:47 +0800

    添加项目文件。

---
+++ PythonApplication1/settings.py
@@ -0,0 +1,11 @@
+class Settings():
+    """"存储《外星人入侵》的所以设置的类"""
+    def __init__(self):
+        """初始化屏幕的设置"""
+        # 屏幕设置
+        self.screen_height = 800
+        self.screen_width = 1200
+        self.bg_color = (230,230,230)
+
+
+
