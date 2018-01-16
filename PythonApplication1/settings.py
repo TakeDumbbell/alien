c0d9b20910d907465526e8d38d1057be30cb0f45
陈环宇 <2399644890@qq.com>
2018-01-16 11:40:41 +0800

    the ship can move now

--- PythonApplication1/settings.py
+++ PythonApplication1/settings.py
@@ -3,9 +3,10 @@ class Settings():
     def __init__(self):
         """初始化屏幕的设置"""
         # 屏幕设置
-        self.screen_height = 800
-        self.screen_width = 1200
+        self.screen_height = 500
+        self.screen_width = 900
         self.bg_color = (230,230,230)
-
+        #飞船的设置
+        self.ship_speed_factor = 1.5
 
 
